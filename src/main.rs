f0d109fa939ed59949150eb47feca6b17b8cfd23
binotation <68807919+binotation@users.noreply.github.com>
2024-10-10 22:24:15 +1000

    Remove static mut, use UnsafeCell

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![no_std]
 #![no_main]
 
+use core::cell::UnsafeCell;
 use cortex_m_rt::entry;
 use heapless::{spsc::Queue, Vec};
 use nrf24l01_commands::{commands, commands::Command, registers};
@@ -12,14 +13,38 @@ use stm32l4::stm32l4x2::{interrupt, Interrupt, Peripherals, SPI1, USART2};
 const TX_ADDR: u64 = 0xA2891FFF6A;
 const PAYLOAD: [u8; 32] = [97; 32];
 
-static mut USART2_PERIPHERAL: Option<USART2> = None;
-static mut SPI1_PERIPHERAL: Option<SPI1> = None;
+struct SyncUnsafeCell<P>(UnsafeCell<Option<P>>);
+
+impl<P> SyncUnsafeCell<P> {
+    const fn new() -> Self {
+        SyncUnsafeCell(UnsafeCell::new(None))
+    }
+
+    fn set(&self, inner: P) {
+        unsafe { *self.0.get() = Some(inner) };
+    }
+
+    fn get(&self) -> &mut P {
+        unsafe { &mut *self.0.get() }.as_mut().unwrap()
+    }
+}
+
+// SAFETY: CPU is single-threaded. Interrupts cannot execute simultaneously and cannot
+// preempt each other (all interrupts have same priority).
+unsafe impl Sync for SyncUnsafeCell<USART2> {}
+unsafe impl Sync for SyncUnsafeCell<SPI1> {}
+unsafe impl Sync for SyncUnsafeCell<Queue<u8, 64>> {}
+unsafe impl Sync for SyncUnsafeCell<Queue<u16, 64>> {}
+unsafe impl Sync for SyncUnsafeCell<Queue<Vec<u8, 64>, 16>> {}
+
+static USART2_PERIPHERAL: SyncUnsafeCell<USART2> = SyncUnsafeCell::new();
+static SPI1_PERIPHERAL: SyncUnsafeCell<SPI1> = SyncUnsafeCell::new();
 /// Bytes to be transmitted over SPI1
-static mut TX_BUFFER: Option<Queue<u8, 64>> = None;
+static TX_BUFFER: SyncUnsafeCell<Queue<u8, 64>> = SyncUnsafeCell::new();
 /// Bytes received over SPI1
-static mut RX_BUFFER: Option<Queue<u16, 64>> = None;
+static RX_BUFFER: SyncUnsafeCell<Queue<u16, 64>> = SyncUnsafeCell::new();
 /// Queued nRF24l01 commands
-static mut COMMANDS: Option<Queue<Vec<u8, 64>, 16>> = None;
+static COMMANDS: SyncUnsafeCell<Queue<Vec<u8, 64>, 16>> = SyncUnsafeCell::new();
 
 #[inline(always)]
 fn enqueue_command(queue: &mut Queue<Vec<u8, 64>, 16>, command_bytes: &[u8]) {
@@ -30,11 +55,10 @@ fn enqueue_command(queue: &mut Queue<Vec<u8, 64>, 16>, command_bytes: &[u8]) {
 
 #[interrupt]
 fn USART2() {
-    // SAFETY: race condition where USART2_PERIPHERAL can be accessed before being set
-    let usart2 = unsafe { USART2_PERIPHERAL.as_mut() }.unwrap();
-    let spi1 = unsafe { SPI1_PERIPHERAL.as_mut() }.unwrap();
-    let rx_buffer = unsafe { RX_BUFFER.as_mut() }.unwrap();
-    let command_queue = unsafe { COMMANDS.as_mut() }.unwrap();
+    let usart2 = USART2_PERIPHERAL.get();
+    let spi1 = SPI1_PERIPHERAL.get();
+    let rx_buffer = RX_BUFFER.get();
+    let command_queue = COMMANDS.get();
 
     // Dequeue bytes off rx buffer and transmit over USART2
     if usart2.isr().read().txe().bit_is_set() {
@@ -107,7 +131,7 @@ fn USART2() {
                 // Write payload
                 enqueue_command(
                     command_queue,
-                    &commands::WriteTxPayloadNoAck::bytes(PAYLOAD),
+                    &commands::WriteTxPayloadNoAck(PAYLOAD).bytes(),
                 );
             }
             104 => {
@@ -131,11 +155,11 @@ fn USART2() {
 
 #[interrupt]
 fn SPI1() {
-    let spi1 = unsafe { SPI1_PERIPHERAL.as_mut() }.unwrap();
-    let usart2 = unsafe { USART2_PERIPHERAL.as_mut() }.unwrap();
-    let tx_buffer = unsafe { TX_BUFFER.as_mut() }.unwrap();
-    let rx_buffer = unsafe { RX_BUFFER.as_mut() }.unwrap();
-    let command_queue = unsafe { COMMANDS.as_mut() }.unwrap();
+    let spi1 = SPI1_PERIPHERAL.get();
+    let usart2 = USART2_PERIPHERAL.get();
+    let tx_buffer = TX_BUFFER.get();
+    let rx_buffer = RX_BUFFER.get();
+    let command_queue = COMMANDS.get();
 
     // Transmit bytes from tx buffer
     if spi1.sr().read().txe().bit_is_set() {
@@ -192,7 +216,9 @@ fn main() -> ! {
 
     // Enable peripheral clocks - GPIOA, USART2
     dp.RCC.ahb2enr().write(|w| w.gpioaen().set_bit());
-    dp.RCC.apb1enr1().write(|w| w.usart2en().enabled());
+    dp.RCC
+        .apb1enr1()
+        .write(|w| w.usart2en().enabled().tim2en().set_bit());
     dp.RCC.apb2enr().write(|w| w.spi1en().set_bit());
 
     // USART2: A2 (TX), A3 (RX) as AF 7
@@ -283,7 +309,7 @@ fn main() -> ! {
     });
 
     // Initial commands
-    let mut command_queue = Queue::default();
+    let mut command_queue = Queue::new();
 
     enqueue_command(
         &mut command_queue,
@@ -333,27 +359,21 @@ fn main() -> ! {
         &commands::ReadRegister::<registers::Config>::bytes(),
     );
 
+    COMMANDS.set(command_queue);
+    TX_BUFFER.set(Queue::new());
+    RX_BUFFER.set(Queue::new());
     unsafe {
-        COMMANDS = Some(command_queue);
-        TX_BUFFER = Some(Queue::default());
-        RX_BUFFER = Some(Queue::default());
-        // Unmask NVIC USART2 global interrupt
+        // Unmask NVIC global interrupts
         cortex_m::peripheral::NVIC::unmask(Interrupt::SPI1);
         cortex_m::peripheral::NVIC::unmask(Interrupt::USART2);
-        SPI1_PERIPHERAL = Some(dp.SPI1);
-        USART2_PERIPHERAL = Some(dp.USART2);
-        // Enable SPI to send initial commands
-        SPI1_PERIPHERAL
-            .as_mut()
-            .unwrap()
-            .cr2()
-            .modify(|_, w| w.txeie().set_bit());
-        SPI1_PERIPHERAL
-            .as_mut()
-            .unwrap()
-            .cr1()
-            .modify(|_, w| w.spe().enabled());
     }
+    SPI1_PERIPHERAL.set(dp.SPI1);
+    USART2_PERIPHERAL.set(dp.USART2);
+
+    // Enable SPI to send initial commands
+    let spi1 = SPI1_PERIPHERAL.get();
+    spi1.cr2().modify(|_, w| w.txeie().set_bit());
+    spi1.cr1().modify(|_, w| w.spe().enabled());
 
     #[allow(clippy::empty_loop)]
     loop {}
